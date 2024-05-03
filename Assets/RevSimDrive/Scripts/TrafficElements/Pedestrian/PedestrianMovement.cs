1b703c2a3abc0654d9483feeedc1ceab2888941c
TomDerks <459530@student.fontys.nl>
2024-05-03 16:39:46 +0200

    Add script for waypoint following
    Add script and spawn for pedestrians that follow waypoints.

---
+++ Assets/RevSimDrive/Scripts/TrafficElements/Pedestrian/PedestrianMovement.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PedestrianMovement : MonoBehaviour
+{
+    public List<Transform> waypoints;
+
+    [SerializeField] private float movespeed = 3f;
+
+    [SerializeField] private float distanceThreshold = 0.1f;
+
+    private Transform targetWaypoint;
+    private int count = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetWaypoint = waypoints[count].transform;
+        transform.LookAt(waypoints[count].transform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, targetWaypoint.position, movespeed * Time.deltaTime);
+
+        if(Vector3.Distance(transform.position, targetWaypoint.position) < distanceThreshold)
+        {
+            if (count + 1 !< waypoints.Count)
+            {
+                count++;
+                targetWaypoint = waypoints[count];
+                transform.LookAt(waypoints[count].transform);
+            }
+            else
+            {
+                count = 0;
+                targetWaypoint = waypoints[count];
+                transform.LookAt(waypoints[count].transform);
+            }
+        }
+
+    }
+}
