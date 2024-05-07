28f9b27308b56defd128bb77b70967eb5597ecbc
TomDerks <459530@student.fontys.nl>
2024-05-07 14:56:53 +0200

    Add bird package and fix script waiting function
    Add inmotionVR bird package, fix pedestrianmovement script to wait at crossroads.

--- Assets/RevSimDrive/Scripts/TrafficElements/Pedestrian/PedestrianMovement.cs
+++ Assets/RevSimDrive/Scripts/TrafficElements/Pedestrian/PedestrianMovement.cs
@@ -12,6 +12,7 @@ public class PedestrianMovement : MonoBehaviour
 
     private Transform targetWaypoint;
     private int count = 1;
+    private bool rotating = false;
 
     void Start()
     {
@@ -30,19 +31,68 @@ public class PedestrianMovement : MonoBehaviour
                 count++;
                 targetWaypoint = waypoints[count];
                 transform.LookAt(waypoints[count].transform);
+
+                StartCoroutine(CrossTheRoad(waypoints[count -1], waypoints[count], 1f));
             }
             else
             {
                 count = 0;
                 targetWaypoint = waypoints[count];
                 transform.LookAt(waypoints[count].transform);
+                StartCoroutine(CrossTheRoad(waypoints[count - 1], waypoints[count], 1f));
             }
         }
 
     }
 
-    private void CrossTheRoad()
+    IEnumerator CrossTheRoad(Transform current, Transform target, float duration)
     {
+        if(target.tag == "CrossingWaypoint" && current.tag == "CrossingWaypoint")
+        {
+            float oldMoveSpeed = movespeed;
+            movespeed = 0f;
+            Quaternion baseRot = transform.rotation;
+            Quaternion newRotR = baseRot  * Quaternion.Euler(new Vector3(0, 90, 0));
+            Quaternion newRotL = baseRot * Quaternion.Euler(new Vector3(0, -90, 0));
 
+            //LookRight
+            float counter = 0;
+            while (counter < duration)
+            {
+                counter += Time.deltaTime;
+                transform.rotation = Quaternion.Lerp(baseRot, newRotR, counter / duration);
+                yield return null;
+            }
+
+            //LookLeft
+            float counter2a = 0;
+            while (counter2a < duration)
+            {
+                counter2a += Time.deltaTime;
+                transform.rotation = Quaternion.Lerp(newRotR, baseRot, counter2a / duration);
+                yield return null; 
+            }
+
+
+            float counter2b = 0;
+            while (counter2b < duration)
+            {
+                counter2b += Time.deltaTime;
+                transform.rotation = Quaternion.Lerp(baseRot, newRotL, counter2b / duration);
+                yield return null;
+            }
+
+
+            //LookForward
+            float counter3 = 0;
+            while (counter3 < duration)
+            {
+                counter3 += Time.deltaTime;
+                transform.rotation = Quaternion.Lerp(newRotL, baseRot, counter3/ duration);
+                yield return null;
+            }
+
+            movespeed = oldMoveSpeed;
+        }
     }
 }
