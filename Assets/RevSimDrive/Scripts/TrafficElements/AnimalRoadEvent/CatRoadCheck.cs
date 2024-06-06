90ce3d1abbad16d035ce442ec4568f743d974ff8
TomDerks <459530@student.fontys.nl>
2024-06-06 16:45:13 +0200

    Add animal event Cat
    Added an gameobject that lets a cat cross the street when the player gets close

---
+++ Assets/RevSimDrive/Scripts/TrafficElements/AnimalRoadEvent/CatRoadCheck.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatRoadCheck : MonoBehaviour
+{
+    [SerializeField] public float moveSpeed = 1f;
+    private Animator animator;
+    private bool isWalking = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = transform.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isWalking)
+        {
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.tag);
+        if(other.tag == "Border")
+        {
+            StopMoving();
+            Debug.Log("Stopeed walking");
+        }
+    }
+
+    public void MoveForward()
+    {
+        animator.SetTrigger("Walk");
+        isWalking = true;
+    }
+
+    private void StopMoving()
+    {
+        animator.SetTrigger("Idle");
+        isWalking = false;
+    }
+}
